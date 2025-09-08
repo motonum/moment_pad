9a88cab7228309c8c8532d9813838ad72bbc3cd3
motonum <73626946+motonum@users.noreply.github.com>
2025-09-08 19:21:04 +0900

    refactor: ショートカットキーの設定をカスタムフックに

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import { useCallback } from "react";
 import "./App.css";
 import useTextWithStore from "./hooks/useTextWithStore";
 import useCopy from "./hooks/useCopy";
+import useShortcutKey from "./hooks/useShortcutKey";
 
 function App() {
   const [text, setTextWithStore] = useTextWithStore();
   const { isCopied, handleCopy } = useCopy({ windowLabel: "main" });
 
-  const handleShortcutKey = useCallback(
-    (e: React.KeyboardEvent) => {
-      const selection = getSelection();
-      if (selection && selection.toString() !== "") {
-        return;
-      }
-
-      const ua = navigator.userAgent.toLowerCase();
-      const isWin = ua.indexOf("windows nt") !== -1;
-      const isMac = ua.indexOf("mac os x") !== -1;
-
-      const modKeys = ["metaKey", "ctrlKey", "altKey", "shiftKey"] as const;
-      const keyBinding: { key: string } & {
-        [k in (typeof modKeys)[number]]: boolean;
-      } = {
-        key: "c",
-        metaKey: isMac, // macならCommandキー
-        ctrlKey: isWin, // windowsならControlキー
-        altKey: false,
-        shiftKey: false,
-      };
-
-      if ((["key", ...modKeys] as const).every((k) => e[k] === keyBinding[k])) {
-        handleCopy(text);
-      }
-    },
-    [handleCopy, text]
-  );
+  useShortcutKey({ key: "c", cmdKey: true }, handleCopy, text);
 
   return (
     <div className="container">
@@ -52,7 +25,6 @@ function App() {
         className="memo-pad"
         value={text}
         onChange={(e) => setTextWithStore(e.target.value)}
-        onKeyDown={handleShortcutKey}
         placeholder="Write something..."
         autoFocus={true}
       />
---
+++ src/hooks/useShortcutKey.ts
@@ -0,0 +1,51 @@
+import { useCallback, useEffect } from "react";
+
+const nativeModKeys = ["metaKey", "ctrlKey", "altKey", "shiftKey"] as const;
+type KeyBinding = { key: string } & {
+  [k in (typeof nativeModKeys)[number]]: boolean;
+};
+
+const shortcutModKeys = ["cmdKey", "altKey", "shiftKey"] as const;
+type HotKey = { key: string } & {
+  [k in (typeof shortcutModKeys)[number]]?: boolean;
+};
+
+const useShortcutKey = <T>(
+  hotKey: HotKey,
+  callback: (...args: T[]) => void,
+  ...args: T[]
+) => {
+  const eventHandler = useCallback(
+    (e: KeyboardEvent) => {
+      const ua = navigator.userAgent.toLowerCase();
+      const isWin = ua.indexOf("windows nt") !== -1;
+      const isMac = ua.indexOf("mac os x") !== -1;
+
+      const keyBinding: KeyBinding = {
+        key: hotKey.key,
+        metaKey: isMac && !!hotKey.cmdKey,
+        ctrlKey: isWin && !!hotKey.cmdKey,
+        altKey: !!hotKey.altKey,
+        shiftKey: !!hotKey.shiftKey,
+      };
+
+      if (
+        (["key", ...nativeModKeys] as const).every(
+          (k) => e[k] === keyBinding[k]
+        )
+      ) {
+        callback(...args);
+      }
+    },
+    [hotKey, callback, args]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", eventHandler);
+    return () => {
+      window.removeEventListener("keydown", eventHandler);
+    };
+  }, [eventHandler]);
+};
+
+export default useShortcutKey;
