2010658bf9d8d2452cd555dc694fe2c18f35bd0e
motonum <73626946+motonum@users.noreply.github.com>
2025-09-08 15:40:46 +0900

    refactor: カスタムフックを別ファイルに

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,7 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { Store } from "@tauri-apps/plugin-store";
-
-const useTextWithStore = (initialText: string = "") => {
-  const [text, setText] = useState<string>(initialText);
-  const [store, setStore] = useState<Store | null>(null);
-
-  useEffect(() => {
-    const init = async () => {
-      const loaded = await Store.load("store.json");
-      setStore(loaded);
-    };
-    init();
-  }, []);
-
-  useEffect(() => {
-    if (!store) return;
-    const load = async () => {
-      const savedText = await store.get<{ value: string }>("text");
-      if (savedText) {
-        setText(savedText.value);
-      }
-    };
-    load();
-  }, [store]);
-
-  const setTextWithStore = useCallback(
-    (text: string) => {
-      setText(text);
-      if (!store) return;
-      const save = async () => {
-        await store.set("text", { value: text });
-        await store.save();
-      };
-      save();
-    },
-    [store]
-  );
-
-  return [text, setTextWithStore] as const;
-};
+import useTextWithStore from "./hooks/useTextWithStore";
 
 function App() {
   const [text, setTextWithStore] = useTextWithStore();
@@ -77,8 +38,8 @@ function App() {
         [k in (typeof modKeys)[number]]: boolean;
       } = {
         key: "c",
-        metaKey: isMac,
-        ctrlKey: isWin,
+        metaKey: isMac, // macならCommandキー
+        ctrlKey: isWin, // windowsならControlキー
         altKey: false,
         shiftKey: false,
       };
---
+++ src/hooks/useTextWithStore.ts
@@ -0,0 +1,43 @@
+import { Store } from "@tauri-apps/plugin-store";
+import { useState, useEffect, useCallback } from "react";
+
+const useTextWithStore = (initialText: string = "") => {
+  const [text, setText] = useState<string>(initialText);
+  const [store, setStore] = useState<Store | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const loaded = await Store.load("store.json");
+      setStore(loaded);
+    };
+    init();
+  }, []);
+
+  useEffect(() => {
+    if (!store) return;
+    const load = async () => {
+      const savedText = await store.get<{ value: string }>("text");
+      if (savedText) {
+        setText(savedText.value);
+      }
+    };
+    load();
+  }, [store]);
+
+  const setTextWithStore = useCallback(
+    (text: string) => {
+      setText(text);
+      if (!store) return;
+      const save = async () => {
+        await store.set("text", { value: text });
+        await store.save();
+      };
+      save();
+    },
+    [store]
+  );
+
+  return [text, setTextWithStore] as const;
+};
+
+export default useTextWithStore;
