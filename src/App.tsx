7fb6d01db4fdfe988796dd2042831ba7d4e11068
motonum <73626946+motonum@users.noreply.github.com>
2025-09-11 18:49:29 +0900

    refactor: early-return

--- src/App.tsx
+++ src/App.tsx
@@ -21,30 +21,28 @@ function App() {
 
   const handleKeyDown = useCallback(
     (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-      if (e.key === "Tab") {
-        e.preventDefault();
+      if (e.key !== "Tab") return;
+      e.preventDefault();
 
-        const textarea = textareaRef.current;
-        if (textarea) {
-          const start = textarea.selectionStart;
-          const end = textarea.selectionEnd;
+      const textarea = textareaRef.current;
+      if (!textarea) return;
 
-          const numSpace = 2;
-          const spaceStr = Array(numSpace).fill(" ").join("");
-          console.log(spaceStr);
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
 
-          const newText = `${text.substring(
-            0,
-            start
-          )}${spaceStr}${text.substring(end)}`;
-          setTextWithStore(newText);
+      const numSpace = 2;
+      const spaceStr = Array(numSpace).fill(" ").join("");
+      console.log(spaceStr);
 
-          setTimeout(() => {
-            textarea.selectionStart = start + numSpace;
-            textarea.selectionEnd = start + numSpace;
-          }, 0);
-        }
-      }
+      const newText = `${text.substring(0, start)}${spaceStr}${text.substring(
+        end
+      )}`;
+      setTextWithStore(newText);
+
+      setTimeout(() => {
+        textarea.selectionStart = start + numSpace;
+        textarea.selectionEnd = start + numSpace;
+      }, 0);
     },
     [text, setTextWithStore]
   );
