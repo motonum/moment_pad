f8c9053ba3ed6f0f35691e6690c6f7938041b361
motonum <73626946+motonum@users.noreply.github.com>
2025-10-05 12:26:08 +0900

    fix: CheckIconのaria-labelを修正

--- src/icons/CheckIcon.tsx
+++ src/icons/CheckIcon.tsx
@@ -9,7 +9,7 @@ function CheckIcon({ color, size }: Props) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 -960 960 960"
-      aria-label="copy icon"
+      aria-label="check icon"
       width={size ?? 24}
       height={size ?? 24}
     >
